f25be71e61acf2bc66d14cab1e314bf6ea8f7f00
OptiJava <OptiJava@protonmail.com>
2026-01-17 11:03:17 +0800

    feat: init

---
+++ DatabasePlugin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using sharwapi.Contracts.Core;
+
+namespace sharwapi.Plugin.database;
+
+public class DatabasePlugin : IApiPlugin
+{
+    public string Name => "database";
+    public string DisplayName => "Database";
+    public string Version => "0.1.0";
+    
+    public void Configure(WebApplication app) { }
+    public void RegisterServices(IServiceCollection services, IConfiguration configuration) { }
+
+    public void RegisterRoutes(IEndpointRouteBuilder app, IConfiguration configuration)
+    {
+        var group = app.MapGroup("/database");
+    }
+}
